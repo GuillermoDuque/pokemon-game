6c7dc2e199910481418bddaa52164292b7622e60
Guillermo <guillermo.duque@solem.cl>
2022-08-13 17:42:15 -0400

    ADD: se agrega funcionalidad para seleccionar un pokemon aleatoreamente de los seleccionados y representarlo en la imagen

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,11 @@
 <template>
-  <h1>¿Quién es ese pokemon?</h1>
-  <PokemonPicture :pokemonId="4" :showPokemon="false" />
+<h1 v-if="!pokemon">Espere por favor...</h1>
+<div v-else>
+ <h1>¿Quién es ese pokemon?</h1>
+  <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
   <PokemonOption :pokemons="pokemonArr"/>
+</div>
+ 
 </template>
 <script>
 import PokemonPicture from '@/components/PokemonPicture.vue'
@@ -15,13 +19,17 @@ export default {
     components:{PokemonPicture, PokemonOption},
     data(){
       return{
-        pokemonArr: []
+        pokemonArr: [],
+        pokemon: null, 
+        showPokemon: false
       }
     },
     methods:{
       async mixPokemonArray(){
           this.pokemonArr = await getPokemonOptions() 
-          console.log(this.pokemonArr)
+          
+          const rndInt = Math.floor( Math.random() * 4)
+          this.pokemon = this.pokemonArr[rndInt]
       }
     },
     mounted(){
