226dc091804505a08dab8cc911f358a64f7e8896
Guillermo <guillermo.duque@solem.cl>
2022-08-18 06:36:20 -0400

    ADD: se agregan test a PokemonPage

---
+++ src/tests/unit/pages/PokemonPage.spec.js
@@ -0,0 +1,25 @@
+import {shallowMount} from '@vue/test-utils'
+import PokemonPage  from '@/pages/PokemonPage'
+ /* eslint-disable no-undef */
+describe('PokemonPageComponent', () =>{
+
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount( PokemonPage )
+    }) 
+    
+   
+
+    test('debe de hacer match con el snapshot', () =>{
+        expect( wrapper.html() ).toMatchSnapshot()
+    })
+
+    test('debe de llamar al mixPokemonArray al montar', () =>{
+        const mixPokemonArraySpy = jest.spyOn( PokemonPage.methods, 'mixPokemonArray' )
+        shallowMount(PokemonPage)
+        expect( mixPokemonArraySpy ).toHaveBeenCalled
+    })
+  
+
+})
